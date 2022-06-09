3112bd1d86552e49568c8fa2b0d66c209851554d
Suhas <kashyap.suhas@protonmail.com>
2022-06-09 19:52:17 +0530

    Project-1: Added function to store logs, and added defer script for smooth/faster execution

--- assets/scripts/calcApp.js
+++ assets/scripts/calcApp.js
@@ -2,40 +2,59 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
+// Gets input from the input field
 function userInputNumber() {
   return parseInt(userInput.value);
 }
-function operationLog(operator, prevResult, inputNumber) {
+
+// Displays the operation that the calculator performed
+function displayOperation(operator, prevResult, inputNumber) {
   const calcDescription = `${prevResult} ${operator} ${inputNumber}`;
-  outputResult(currentResult, calcDescription);
+  outputResult(currentResult, calcDescription); // From the vendor.js file
+}
+
+// Stores the logs from every operation
+function storeLog(inpNum, operand, origRes, endRes) {
+  const logEntry = {
+    number: inpNum,
+    operation: operand,
+    origResult: origRes,
+    endResult: endRes
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
 function add() {
   const enteredNumber = userInputNumber();
   const initialResult = currentResult;
   currentResult += enteredNumber;
-  operationLog('+', initialResult, enteredNumber);
+  displayOperation('+', initialResult, enteredNumber);
+  storeLog(enteredNumber, 'ADD', initialResult, currentResult);
 }
 
 function subtract() {
   const enteredNumber = userInputNumber();
   const initialResult = currentResult;
   currentResult -= enteredNumber;
-  operationLog('-', initialResult, enteredNumber);
+  displayOperation('-', initialResult, enteredNumber);
+  storeLog(enteredNumber, 'SUBTRACT', initialResult, currentResult);
 }
 
 function multiply() {
   const enteredNumber = userInputNumber();
   const initialResult = currentResult;
   currentResult *= enteredNumber;
-  operationLog('*', initialResult, enteredNumber);
+  displayOperation('*', initialResult, enteredNumber);
+  storeLog(enteredNumber, 'MULTIPLY', initialResult, currentResult);
 }
 
 function divide() {
   const enteredNumber = userInputNumber();
   const initialResult = currentResult;
   currentResult /= enteredNumber;
-  operationLog('/', initialResult, enteredNumber);
+  displayOperation('/', initialResult, enteredNumber);
+  storeLog(enteredNumber, 'DIVIDE', initialResult, currentResult);
 }
 
 addBtn.addEventListener('click', add);
