835aa3cb7c74605ac9da159fa5a12e0e40863e9b
Suhas <kashyap.suhas@protonmail.com>
2022-06-09 15:50:30 +0530

    Project-1: Added subtract,multiply & divide functions

--- assets/scripts/calcApp.js
+++ assets/scripts/calcApp.js
@@ -12,4 +12,29 @@ function add() {
   outputResult(currentResult, calcDescription);
 }
 
+function subtract() {
+  const enteredNumber = userInputNumber();
+  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+  const enteredNumber = userInputNumber();
+  const calcDescription = `${currentResult} * ${enteredNumber}`;
+  currentResult = currentResult * enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+  const enteredNumber = userInputNumber();
+  const calcDescription = `${currentResult} / ${enteredNumber}`;
+  currentResult = currentResult / enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
