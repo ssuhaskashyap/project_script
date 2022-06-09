5fb6d25abc7c4adde557658b98f9e2e8a62e6024
Suhas <kashyap.suhas@protonmail.com>
2022-06-09 16:25:42 +0530

    Project-1: Updated with entry log function for reusability

--- assets/scripts/calcApp.js
+++ assets/scripts/calcApp.js
@@ -1,36 +1,41 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function userInputNumber() {
   return parseInt(userInput.value);
 }
+function operationLog(operator, prevResult, inputNumber) {
+  const calcDescription = `${prevResult} ${operator} ${inputNumber}`;
+  outputResult(currentResult, calcDescription);
+}
 
 function add() {
   const enteredNumber = userInputNumber();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
-  outputResult(currentResult, calcDescription);
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  operationLog('+', initialResult, enteredNumber);
 }
 
 function subtract() {
   const enteredNumber = userInputNumber();
-  const calcDescription = `${currentResult} - ${enteredNumber}`;
-  currentResult = currentResult - enteredNumber;
-  outputResult(currentResult, calcDescription);
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  operationLog('-', initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = userInputNumber();
-  const calcDescription = `${currentResult} * ${enteredNumber}`;
-  currentResult = currentResult * enteredNumber;
-  outputResult(currentResult, calcDescription);
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  operationLog('*', initialResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = userInputNumber();
-  const calcDescription = `${currentResult} / ${enteredNumber}`;
-  currentResult = currentResult / enteredNumber;
-  outputResult(currentResult, calcDescription);
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  operationLog('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
